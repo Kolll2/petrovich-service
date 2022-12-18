816e0853bcc77e74c2531db1ea59f2d64037a56b
Kolll2 <samaraanime@mail.ru>
2022-12-18 22:41:42 +0400

    init

---
+++ src/main/java/com/kosh/petrovichservice/PetrovichServiceApplication.java
@@ -0,0 +1,14 @@
+package com.kosh.petrovichservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class PetrovichServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetrovichServiceApplication.class, args);
+    }
+}
