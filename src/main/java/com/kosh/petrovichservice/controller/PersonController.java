4ee6127ea1d4d21e13dce454a4956cd3d958edb8
Kolll2 <samaraanime@mail.ru>
2022-12-19 00:47:04 +0400

    add swagger description

--- src/main/java/com/kosh/petrovichservice/controller/PersonController.java
+++ src/main/java/com/kosh/petrovichservice/controller/PersonController.java
@@ -2,10 +2,14 @@ package com.kosh.petrovichservice.controller;
 
 import com.kosh.petrovichservice.dto.PersonDto;
 import com.kosh.petrovichservice.service.PersonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/person")
+@Api("Контроллер склонения Имени/Фамилии/Отчества")
 public class PersonController {
     private final PersonService personService;
 
@@ -14,15 +18,22 @@ public class PersonController {
     }
 
     @GetMapping
-    public String getDeclination(@RequestParam String name,
-                                 @RequestParam String nameType,
-                                 @RequestParam String gender,
-                                 @RequestParam String caseName) {
+    @ApiOperation(value = "Склоняет переданный переданную строку согласно параметрам",
+            notes = "Возвращает склоненную строку согласно параметрам")
+    public String getDeclination(@RequestParam @ApiParam(name = "name", value = "Склоняемая строка", example = "Максим") String name,
+                                 @RequestParam @ApiParam(name = "nameType", value = "Тип строки", example = "FirstName",
+                                         allowableValues = "FirstName,LastName,PatronymicName") String nameType,
+                                 @RequestParam @ApiParam(name = "gender", value = "Пол", example = "Male",
+                                         allowableValues = "Male,Female,Both") String gender,
+                                 @RequestParam @ApiParam(name = "caseName", value = "Склонение", example = "Dative",
+                                         allowableValues = "Genitive,Dative,Accusative,Instrumental,Prepositional") String caseName) {
         return personService.getDeclination(name, nameType, gender, caseName);
     }
 
     @PostMapping
-    public String getDeclination(@RequestBody PersonDto personDto) {
+    @ApiOperation(value = "Склоняет переданный переданную строку согласно параметрам (PersonDto)",
+            notes = "Возвращает склоненную строку согласно параметрам")
+    public String getDeclination(@RequestBody @ApiParam(value = "Person dto", required = true) PersonDto personDto) {
         return personService.getDeclination(
                 personDto.getName(),
                 personDto.getNameType(),
--- src/main/java/com/kosh/petrovichservice/dto/PersonDto.java
+++ src/main/java/com/kosh/petrovichservice/dto/PersonDto.java
@@ -1,11 +1,20 @@
 package com.kosh.petrovichservice.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel
 public class PersonDto {
+    @ApiModelProperty(name = "String for declination", value = "Склоняемая строка", example = "Максим", required = true)
     private String name;
+    @ApiModelProperty(name = "Enum name type", value = "Тип строки", example = "FirstName", required = true,
+            allowableValues = "FirstName,LastName,PatronymicName")
     private String nameType;
+    @ApiModelProperty(name = "Пол", value = "Пол", example = "Male", required = true, allowableValues = "Male,Female,Both")
     private String gender;
+    @ApiModelProperty(name = "Склонение", value = "Склонение", example = "Dative", required = true,
+            allowableValues = "Genitive,Dative,Accusative,Instrumental,Prepositional")
     private String caseType;
 }
